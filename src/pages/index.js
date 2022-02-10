181ec55d625f6e8e891a44dae9fae9b4bd0aa055
Mags <4146037+MagsMagnoli@users.noreply.github.com>
2022-02-10 16:40:22 +0000

    add signup form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,36 @@
-import * as React from "react"
 import { Link } from "gatsby"
-import styles from "./index.module.css"
-
+import * as React from "react"
+import DemandList from "../components/DemandList"
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
-import DemandList from "../components/DemandList"
+import SignupForm from "../components/SignupForm"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="May Day Strike" />
-    <div style={{ marginBottom: '3rem' }}>
+    <div style={{ marginBottom: "3rem" }}>
       <h2>Who We Are</h2>
-      <p>May Day Strike is a grassroots movement focused on establishing economic security, dignity, and fairness for workers of every nationality, race, and political affiliation.</p>
-      <p>May 1 will mark the beginning of a general strike, a debt strike, and a series of protests & demonstrations that will last until workers’ demands are met.</p>
+      <p>
+        May Day Strike is a grassroots movement focused on establishing economic
+        security, dignity, and fairness for workers of every nationality, race,
+        and political affiliation.
+      </p>
+      <p>
+        May 1 will mark the beginning of a general strike, a debt strike, and a
+        series of protests & demonstrations that will last until workers’
+        demands are met.
+      </p>
       <p>What are we asking for, exactly?</p>
     </div>
     <DemandList />
-    <p>Join us on Discord and social media to become part of the new era of labor organization in the United States and across the globe.</p>
-    <p><Link to="/faq">Frequently Asked Questions</Link></p>
+    <p>
+      Join us on Discord and social media to become part of the new era of labor
+      organization in the United States and across the globe.
+    </p>
+    <p>
+      <Link to="/faq">Frequently Asked Questions</Link>
+    </p>
+    <SignupForm />
   </Layout>
 )
 
